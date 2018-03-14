76dc5a222a9f322ed54bb7226d65a797e646203d
Sandeep Vijayakumar <sandeep@Sandeeps-MacBook-Pro.local>
2018-03-14 14:56:08 -0500

    initial commit - proj structure

---
+++ src/main/java/com/ska/docsch/DocscheduleApplication.java
@@ -0,0 +1,12 @@
+package com.ska.docsch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocscheduleApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ska/docsch/api/DocScheduleApi.java
@@ -0,0 +1,10 @@
+package com.ska.docsch.api;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by sandeep on 3/14/18.
+ */
+@Service()
+public class DocScheduleApi {
+}

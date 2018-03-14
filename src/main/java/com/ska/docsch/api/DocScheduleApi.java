29e8d2eeff455df0b7425cf5d792226fa2fd7932
Sandeep Vijayakumar <sandeep@Sandeeps-MacBook-Pro.local>
2018-03-14 16:35:26 -0500

    added required dependencies and sample endpoint

--- src/main/java/com/ska/docsch/api/DocScheduleApi.java
+++ src/main/java/com/ska/docsch/api/DocScheduleApi.java
@@ -1,10 +1,25 @@
 package com.ska.docsch.api;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by sandeep on 3/14/18.
  */
-@Service()
+@RestController()
 public class DocScheduleApi {
+
+  @PostMapping("/docschedule/schedule")
+  @Produces(value = {"application/json"})
+  @Consumes(value = {"application/json"})
+  public Response doPost() {
+    return Response.noContent().build();
+  }
 }
